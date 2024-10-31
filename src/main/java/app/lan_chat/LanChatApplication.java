7f944ed7946bdb50062e25ea91ce848b9de88bf2
milan <doslicmilan1@gmail.com>
2024-10-31 19:24:43 +0100

    Initial commit

---
+++ src/main/java/app/lan_chat/LanChatApplication.java
@@ -0,0 +1,13 @@
+package app.lan_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/lan_chat/LanChatApplicationTests.java
@@ -0,0 +1,13 @@
+package app.lan_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
